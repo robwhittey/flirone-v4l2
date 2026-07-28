b13c2cc6f579d2286b8cf3ac96f68e706ea04abc
robwhittey <131608279+robwhittey@users.noreply.github.com>
2026-07-28 14:58:13 +0100

    Update flirone.c

--- src/flirone.c
+++ src/flirone.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015-2016 Thomas <tomas123 @ EEVblog Electronics Community Forum>
- * Modified: Multi-camera support via serial number, thermal-only output, and auto-gain removed
+ * Modified: Multi-camera support via serial number, thermal-only output
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
